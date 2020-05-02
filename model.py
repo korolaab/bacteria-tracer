0119da1e3fdce35a9917ec40d5f08c777f49fc49
korolaab <gusarov.alex3@ya.ru>
2020-05-03 01:17:06 +0300

    migrate to TF2

--- model.py
+++ model.py
@@ -1,11 +1,17 @@
-from keras.models import Model
-from keras import backend as K
-from keras.layers import Input,MaxPooling2D, Conv2D,Dropout,Conv2DTranspose, concatenate,LeakyReLU, BatchNormalization
-from keras.optimizers import Adam
 import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Input,MaxPooling2D, Conv2D,Dropout,Conv2DTranspose, concatenate,LeakyReLU, BatchNormalization
+from tensorflow.keras.optimizers import Adam
 import random
 import math
 import numpy as np
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
 dtype="float32"
 def augmentation(x,y,batch_size=32):#augmentation
     size = x.shape[1]
@@ -33,15 +39,12 @@ def augmentation(x,y,batch_size=32):#augmentation
             y_train[i,:,:,:]=y_temp
         yield x_train,y_train
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.InteractiveSession(config=config)
 
 def adjust_gamma(image, gamma=1.0):
 	image = image**gamma
 	return image
 
-def crop_from_photo(image,size,shift,mode=None):
+def crop_from_photo(image,size,shift,mode=None):# give pieces of images  
      N = math.ceil(image.shape[0]/shift)*math.ceil(image.shape[1]/shift)
      pieces = np.zeros([N,size,size,image.shape[2]],dtype=dtype)
      n=0
@@ -77,7 +80,7 @@ def focal_loss(y_true, y_pred): #https://lars76.github.io/neural-networks/object
         return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 
 def graph(input_shape = (100,100,1),n_filters=8,batchnorm=False,dropout=0.3,
-pretrained_weights=None):
+                    pretrained_weights=None):
     # n_filters - number of conv2D kernels
     inputs = Input(input_shape,dtype="float32")
     c1 = conv2d_block(inputs, n_filters=n_filters*1, kernel_size=3, batchnorm=batchnorm)
@@ -89,18 +92,18 @@ pretrained_weights=None):
     p2 = Dropout(dropout)(p2)
 
     c3 = conv2d_block(p2, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
-    #p3 = MaxPooling2D((2, 2)) (c3)
-    #p3 = Dropout(dropout)(p3)
+    p3 = MaxPooling2D((2, 2)) (c3)
+    p3 = Dropout(dropout)(p3)
 
-    #c4 = conv2d_block(p3, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
+    c4 = conv2d_block(p3, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
 
-    #u5 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same') (c4)
-    #u5 = concatenate([u5, c3])
-    #u5 = Dropout(dropout)(u5)
-    #c5 = conv2d_block(u5, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
+    u5 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same') (c4)
+    u5 = concatenate([u5, c3])
+    u5 = Dropout(dropout)(u5)
+    c5 = conv2d_block(u5, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
 
 
-    u6 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same') (c3)
+    u6 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same') (c5)
     u6 = concatenate([u6, c2])
     u6 = Dropout(dropout)(u6)
     c6 = conv2d_block(u6, n_filters=n_filters*2, kernel_size=3, batchnorm=batchnorm)
